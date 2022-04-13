a353e43ab00413fde592bc72ecc1a8479cf1526d
Sergio Ortiz <ortiz.sergio0303@gmail.com>
2022-04-13 14:18:34 -0400

    feat(results) link to business page with logged business entry

--- pages/success/[id].js
+++ pages/success/[id].js
@@ -1,12 +1,19 @@
 import Layout from "../../components/layout";
 
 export async function getServerSideProps(context) {
+  const results = await prisma.business.findUnique({
+    where: {
+      id: parseInt(context.query.id),
+    },
+  });
+
   return {
-    props: { id: context.query.id },
+    props: { results, id: context.query.id },
   };
 }
 
-export default function Success({ id }) {
+export default function Success({ results, id }) {
+  console.dir(results);
   return (
     <Layout>
       <h2>Business, ID #{id}: added to directory. &#x1F370;&#x1F389;</h2>
