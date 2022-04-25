45a225605b4620b2d44a8aafe4bd6394333f4d18
Sergio Ortiz <ortiz.sergio0303@gmail.com>
2022-04-25 10:36:10 -0400

    feat(home): page create-button flex-container justify-content flex-end

--- components/create-button/index.js
+++ components/create-button/index.js
@@ -1,12 +1,14 @@
 import Link from "next/link";
-import { btn } from "./create-button.module.css";
+import { btn, flexContainer } from "./create-button.module.css";
 
 const CreateButton = () => (
-  <Link href="/create-business">
-    <a>
-      <button className={btn}>Create</button>
-    </a>
-  </Link>
+  <div className={flexContainer}>
+    <Link href="/create-business">
+      <a>
+        <button className={btn}>Create</button>
+      </a>
+    </Link>
+  </div>
 );
 
 export default CreateButton;
