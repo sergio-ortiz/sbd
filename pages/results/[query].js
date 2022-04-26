b9003027bbf37a5f736b0db8f58d0d3a48b100b4
Sergio Ortiz <ortiz.sergio0303@gmail.com>
2022-04-26 16:27:44 -0400

    feat(results): page

--- pages/results/[query].js
+++ pages/results/[query].js
@@ -1,19 +1,45 @@
+import Link from "next/link";
 import prisma from "../../lib/prisma";
+import Layout from "../../components/layout";
 
-export async function getServerSideProps({ query }) {
+export async function getServerSideProps({ params }) {
   const results = await prisma.business.findMany({
     where: {
       names: {
         some: {
-          content: query.query,
+          content: params.query,
         },
       },
     },
+    select: {
+      names: {
+        where: {
+          official: true,
+        },
+      },
+      year: true,
+      id: true,
+    },
   });
 
-  return { props: { results: JSON.stringify(results) } };
+  return { props: { results } };
 }
 
-const results = ({ results }) => <>{results}</>;
+const results = ({ results }) => {
+  return (
+    <Layout>
+      <h2>Matched businesses in directory &#x1F4C1;</h2>
+      <ul>
+        {results.map(({ id, names, year }) => (
+          <li key={id}>
+            <Link href={`/business/${id}`}>
+              <a>{`${names[0].content}, est. ${year}`}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </Layout>
+  );
+};
 
 export default results;
