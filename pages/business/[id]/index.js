76db838d9f5f183c66a5a0f5d7805db0ce9e870c
Sergio Ortiz <ortiz.sergio0303@gmail.com>
2022-05-06 17:46:16 -0400

    refactor(business): page props to be more concise

--- pages/business/[id]/index.js
+++ pages/business/[id]/index.js
@@ -3,46 +3,55 @@ import Layout from "../../../components/layout";
 import LinkItem from "../../../components/link-item";
 
 export async function getServerSideProps(context) {
-  const results = await prisma.business.findUnique({
+  const props = await prisma.business.findUnique({
     where: {
       id: parseInt(context.query.id),
     },
-    include: {
+    select: {
+      id: true,
       names: true,
+      principalAddress: true,
+      mailingAddress: true,
+      year: true,
+      industry: true,
     },
   });
 
-  return {
-    props: { results, id: context.query.id },
-  };
+  return { props };
 }
 
-export default function Success({ results, id }) {
-  console.log(results);
+export default function Success({
+  names,
+  principalAddress,
+  mailingAddress,
+  year,
+  industry,
+  id,
+}) {
   return (
     <Layout>
       <h2>Business, ID #{id} &#x1F454;</h2>
       <h3>Information found in directory</h3>
       <h4>Official Business Name:</h4>
-      <p>{results.names[0].content}</p>
+      <p>{names[0].content}</p>
       <h4>Altenative Names</h4>
 
       {(() => {
-        if (results.names.length > 1) {
-          const altNames = results.names.slice(1);
+        if (names.length > 1) {
+          const altNames = names.slice(1);
           return altNames.map((n) => <p key={n.id}>{n.content}</p>);
         }
       })()}
 
       <h4>Principal Address</h4>
-      <p>{results.principalAddress}</p>
+      <p>{principalAddress}</p>
       <h4>Mailing Address</h4>
-      <p>{results.mailingAddress}</p>
+      <p>{mailingAddress}</p>
 
       <h4>Year Established</h4>
-      <p>{results.year}</p>
+      <p>{year}</p>
       <h4>Industry</h4>
-      <p>{results.industry}</p>
+      <p>{industry}</p>
 
       <ul>
         <LinkItem href={`/business/${id}/misc`} text={"Miscellaneous Info"} />
