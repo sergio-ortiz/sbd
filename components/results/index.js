302c86f09f123f8b1e61aa363997f981872248a3
Sergio Ortiz <ortiz.sergio0303@gmail.com>
2022-05-07 17:27:12 -0400

    refactor(collisions): page results component list child elements to LinkItem component

--- components/results/index.js
+++ components/results/index.js
@@ -1,15 +1,15 @@
-import Link from "next/link";
+import LinkItem from "../link-item";
 
 const Results = ({ results }) => (
   <>
     <h2>Found matching businesses in directory &#x1F4C1;</h2>
     <ul>
       {results.map(({ id, names, year }) => (
-        <li key={id}>
-          <Link href={`/business/${id}`}>
-            <a>{`${names[0].content}, est. ${year}`}</a>
-          </Link>
-        </li>
+        <LinkItem
+          href={`/business/${id}`}
+          text={`${names[0].content}, est. ${year}`}
+          key={id}
+        />
       ))}
     </ul>
   </>
